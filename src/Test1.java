146eba22504bb5a41f939841a3c3d67f68e46e39
Abhijeet <Abhijeet@DESKTOP-G9LQQSU>
2019-12-12 19:16:54 +0530

    1 Commit from workspace to branchV

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,11 @@
 
 public class Test1 {
-
+public static void main(String[] args) {
+	Test1 t1=new Test1();
+}
 	public Test1() {
-		// TODO Auto-generated constructor stub
+		for(int i=0;i<10;i++) {
+			System.out.println("Github"+i);
+		}
 	}
 }
