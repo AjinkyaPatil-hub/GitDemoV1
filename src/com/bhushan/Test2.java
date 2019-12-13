b99337fb994b6065d69db24cb3db4cbff6c15e62
AjinkyaPatil-hub <52791315+AjinkyaPatil-hub@users.noreply.github.com>
2019-12-13 15:09:16 +0530

    Update Test2.java

--- src/com/bhushan/Test2.java
+++ src/com/bhushan/Test2.java
@@ -4,8 +4,8 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		String str="our project";
-		
-		
+		System.out.println("Hiiii");
+
 	}
 	
 	public static void reverseString(String str) {
